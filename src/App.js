3176cffb013a7cc98e1040dc9318803d860d1ad7
Ijaz Bacha <76259605+bacha-11@users.noreply.github.com>
2021-12-23 13:57:35 +0500

    using a useState

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
+import { useState } from "react";
 import Person from "./Person/Person";
 
 
 function App() {
+  const [persons, setPersons] = useState([
+    {name:"ijaz", age:"24"},
+    {name:"bacha", age:"20"},
+    {name:"zaid", age:'10'}
+  ])
   return (
     <div className="App">
      <h1>React Complete Guide</h1>
-     <Person name="ijaz" age="25" />
-     <Person name="bacha" age="20" > Hobbies: Travling </Person>
-     <Person name="zaid" age='10' />
+     <Person name={persons[0].name} age={persons[0].age} />
+     <Person name={persons[1].name} age={persons[1].age} > Hobbies: Travling </Person>
+     <Person name={persons[2].name} age={persons[2].name} />
     </div>
   );
 }
