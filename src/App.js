f1fd8e2af1efd1c2a68100991dbb7ea725f25580
Ijaz Bacha <76259605+bacha-11@users.noreply.github.com>
2021-12-23 14:27:36 +0500

    handle event

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import { useState } from "react";
 import Person from "./Person/Person";
 
-
 function App() {
   const [persons, setPersons] = useState([
-    {name:"ijaz", age:"24"},
-    {name:"bacha", age:"20"},
-    {name:"zaid", age:'10'}
-  ])
+    { name: "ijaz", age: "24" },
+    { name: "bacha", age: "20" },
+    { name: "zaid", age: "10" },
+  ]);
+
+  const switchNameHandler = () => {
+    setPersons([
+      { name: "ijaz bacha", age: "24" },
+      { name: "bacha", age: "20" },
+      { name: "zaid", age: "30" },
+    ]);
+  };
+
   return (
     <div className="App">
-     <h1>React Complete Guide</h1>
-     <Person name={persons[0].name} age={persons[0].age} />
-     <Person name={persons[1].name} age={persons[1].age} > Hobbies: Travling </Person>
-     <Person name={persons[2].name} age={persons[2].name} />
+      <h1>React Complete Guide</h1>
+      <button onClick={switchNameHandler}>SwitchName</button>
+      <Person name={persons[0].name} age={persons[0].age} />
+      <Person name={persons[1].name} age={persons[1].age}>
+        Hobbies: Travling
+      </Person>
+      <Person name={persons[2].name} age={persons[2].age} />
     </div>
   );
 }
