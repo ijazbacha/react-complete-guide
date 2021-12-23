4dd92bb1d160539ada057388747003254175da8d
Ijaz Bacha <76259605+bacha-11@users.noreply.github.com>
2021-12-23 16:46:30 +0500

    add separate file styling

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,8 @@
-
+import "./Person.css"
 
 const person = (props) =>{
     return(
-        <div>
+        <div className="Person">
             <p onClick={props.click}>I'm {props.name} and i'm {props.age}! </p>
             <p> {props.children} </p>
              <input onChange={props.changeHandler} value={props.name} />  {/*// two way binding*/}
