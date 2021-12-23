147114e939e604fda9b4201e786f849ff1d1838f
Ijaz Bacha <76259605+bacha-11@users.noreply.github.com>
2021-12-23 12:55:04 +0500

    dynamic content

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,7 +3,7 @@
 const person = () =>{
     return(
         <div>
-            <h2>This is a Person Component</h2>
+            <h2>This is a Person { Math.floor(Math.random() * 30)  } Component</h2>
         </div>
     )
 }
