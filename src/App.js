92a272810e3b9dca3a1afd57594131de31390f4e
Ijaz Bacha <76259605+bacha-11@users.noreply.github.com>
2021-12-30 16:31:56 +0500

    update item in a list or array

--- src/App.js
+++ src/App.js
@@ -5,25 +5,38 @@ import "./Person/Person.css";
 function App() {
   const [showPerson, setShowPerson] = useState(false);
   const [persons, setPersons] = useState([
-    { name: "ijaz", age: "24" },
-    { name: "bacha", age: "20" },
-    { name: "zaid", age: "10" },
+    { id: "1", name: "ijaz", age: "24" },
+    { id: "2", name: "bacha", age: "20" },
+    { id: "3", name: "zaid", age: "10" },
   ]);
 
 
-  const changeHandler = (e) => {
-    setPersons([
-      { name: "ijaz bacha", age: "24" },
-      { name: e.target.value, age: "20" },
-      { name: "zaid", age: "30" },
-    ]);
+  const changeHandler = (e, id) => {
+    const personIndex = persons.findIndex((person) => person.id == id)
+    // console.log("personIndex", personIndex)
+
+    const person = {...persons[personIndex]}
+    console.log('person', person)
+    person.name = e.target.value
+
+    const personsData = [...persons]
+    console.log("persons", personsData)
+    personsData[personIndex] = person
+    console.log("persons2", personsData)
+
+    setPersons([...personsData])
+    // setPersons([
+    //   { name: "ijaz bacha", age: "24" },
+    //   { name: e.target.value, age: "20" },
+    //   { name: "zaid", age: "30" },
+    // ]);
   };
 
   const  deletePersonHandler = (personIndex) =>{
     const data = [...persons]
     console.log(data)
     data.splice(personIndex, 1)
-    console.log("delete person", data)
+    // console.log("delete person", data)
     setPersons([...data])
   }
 
@@ -34,17 +47,13 @@ function App() {
       <div style={{ width: "100%" }}>
         {
           persons.map((person, index) =>(
-            <Person key={index} name={person.name} age={person.age}  click={() => deletePersonHandler(index)} />
+            <Person key={index} name={person.name} age={person.age} changeHandler={(e) => changeHandler(e, person.id)}  click={() => deletePersonHandler(index)} />
           ))
         }
           
         </div>
     )
   }
-
-  console.log('====================================');
-  console.log("persons", persons);
-  console.log('====================================');
 
   return (
     <div className="App">
